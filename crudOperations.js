c8fbd1ed5c560bfc6f749bc083937d18114038ec
Mehil Shah <shahmehil6@gmail.com>
2021-10-20 09:21:27 +0530

    Mongoose ODM

--- crudOperations.js
+++ crudOperations.js
@@ -2,32 +2,20 @@ const assert = require('assert');
 
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        console.log("Inserted " + result.insertedCount +
-            " documents into the collection " + collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        callback(docs);        
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        console.log("Removed the document ", document);
-        callback(result);        
-    });
+    return coll.deleteOne(document);
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        console.log("Updated the document with ", update);
-        callback(result);        
-    });
+    return coll.updateOne(document, { $set : update}, null);
 };
--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'conFusion';
+const Dishes = require('./models/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
 
-const crudOperations = require('./crudOperations');
-
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
-    console.log("Connected to Server.");
-    const db = client.db(dbName);
-    crudOperations.insertDocument(db, { name: "Vadonut", description: "Test"},
-    "dishes", (result) => {
-        console.log("Insert Document:\n", result.ops);
-        crudOperations.findDocuments(db, "dishes", (docs) => {
-            console.log("Found Documents:\n", docs);
-            crudOperations.updateDocument(db, { name: "Vadonut" },
-            { description: "Updated Test" }, "dishes",
-            (result) => {
-                console.log("Updated Document:\n", result.result);
-                crudOperations.findDocuments(db, "dishes", (docs) => {
-                    console.log("Found Updated Documents:\n", docs);
-                    db.dropCollection("dishes", (result) => {
-                        console.log("Dropped Collection: ", result);
-                        client.close();
-                    });
-                });
-            });
+connect.then((db) => {
+    console.log("Connected to the server!");
+    
+    Dishes.create({
+        name : 'Uthapizza',
+        description : 'test'
+    }).then((dish) => {
+        console.log('Inserted : ' + dish);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set : { description : 'Updated Test'}
+        }, {
+            new : true
+        }).exec();
+    }).then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
         });
+        return dish.save();
+    }).then((dishes) => {
+        console.log('Deleted : ' + dishes);
+        return Dishes.deleteMany({});
+    }).then(() => {
+        return mongoose.connection.close();
+    }).catch((err) => {
+        console.log(err);
     });
 });
